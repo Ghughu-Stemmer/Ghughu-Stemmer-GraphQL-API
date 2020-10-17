0be66ec933306f48c4864dc29cc191c7bd6c2c3f
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2020-10-17 22:06:31 +0600

    Add basic GraphQL settings

--- GhughuServer/settings.py
+++ GhughuServer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'ghughu.schema.schema',
+}
