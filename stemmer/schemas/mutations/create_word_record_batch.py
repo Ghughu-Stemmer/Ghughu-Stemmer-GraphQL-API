8e2a4673d4d329b4a138f198e02feea5d86f46ff
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2020-10-18 18:23:27 +0600

    Add duplicate entry prevention in CreateWordRecordBatch

--- stemmer/schemas/mutations/create_word_record_batch.py
+++ stemmer/schemas/mutations/create_word_record_batch.py
@@ -6,13 +6,27 @@ from stemmer.models import WordRecord
 from stemmer.schemas.types import WordRecordType
 
 
+def findDuplicateRecords(wordRecords):
+    inputWords = [record.inflectionalWord for record in wordRecords]
+    return WordRecord.objects.filter(
+        inflectionalWord__in=inputWords
+    )
+
+
+def removeDuplicateRecords(wordRecords):
+    duplicateWords = [record.inflectionalWord for record in findDuplicateRecords(wordRecords)]
+    wordRecords = filter(lambda record: record.inflectionalWord not in duplicateWords, wordRecords)
+
+    return wordRecords
+
+
 class CreateWordRecordBatch(graphene.Mutation):
     Output = graphene.List(WordRecordType)
 
     class Arguments:
-        words = graphene.String()
+        records = graphene.String()
 
-    def mutate(self, info, words):
+    def mutate(self, info, records):
         wordRecords = [WordRecord(
             inflectionalWord=word.get("inflectionalWord", None),
             isVerb=word.get("isVerb", None),
@@ -23,14 +37,10 @@ class CreateWordRecordBatch(graphene.Mutation):
             targetStemWord=word.get("targetStemWord", None),
             isAmbiguous=word.get("isAmbiguous", False),
             comment=word.get("comment", None)
-        ) for word in json.loads(words)]
+        ) for word in json.loads(records)]
+
+        wordRecords = removeDuplicateRecords(wordRecords)
 
         WordRecord.objects.bulk_create(wordRecords)
 
-        inputWords = [record.inflectionalWord for record in wordRecords]
-
-        savedWordRecords = WordRecord.objects.filter(
-            inflectionalWord__in=inputWords
-        )
-
-        return savedWordRecords
+        return findDuplicateRecords(wordRecords)
